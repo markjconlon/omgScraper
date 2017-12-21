1cbfc413655dbb0dacb642b4b69966aa4a9b3f9e
Mark Conlon <markjconlon@gmail.com>
2017-12-20 20:35:58 -0800

    quick and dirty begin rescue block to avoid timeout

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -8,11 +8,20 @@ class TradesController < ApplicationController
   end
 
   def check_trades
-    50.times do
-      liqui_response = HTTParty.get('https://api.liqui.io/api/3/depth/omg_eth?limit=10')
-      poloniex_response = HTTParty.get('https://poloniex.com/public?command=returnOrderBook&currencyPair=ETH_OMG&depth=10')
-      puts "working"
-
+    x = 25
+    x.times do
+      begin
+        liqui_response = HTTParty.get('https://api.liqui.io/api/3/depth/omg_eth?limit=10')
+      rescue Errno::ETIMEDOUT
+        liqui_response = HTTParty.get('https://api.liqui.io/api/3/depth/omg_eth?limit=10')
+      end
+      begin
+        poloniex_response = HTTParty.get('https://poloniex.com/public?command=returnOrderBook&currencyPair=ETH_OMG&depth=10')
+      rescue Errno::ETIMEDOUT
+        poloniex_response = HTTParty.get('https://poloniex.com/public?command=returnOrderBook&currencyPair=ETH_OMG&depth=10')
+      end
+      puts "///// #{x} ///////"
+      x -= 1;
       Trade.check_trades(liqui_response, poloniex_response)
 
       sleep(rand(5..9))
