80d5ba70f195551069fbfd636915670812cd8cf3
markjconlon <markjconlon@gmail.com>
2017-12-23 15:53:07 -0500

    fixed the incorrectly written begin, resuce, retry blocks

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -8,17 +8,19 @@ class TradesController < ApplicationController
   end
 
   def check_trades
-    x = 25
+    x = 600
     x.times do
       begin
         liqui_response = HTTParty.get('https://api.liqui.io/api/3/depth/omg_eth?limit=10')
       rescue Errno::ETIMEDOUT
-        liqui_response = HTTParty.get('https://api.liqui.io/api/3/depth/omg_eth?limit=10')
+        puts "poloniex rescue"
+        retry
       end
       begin
         poloniex_response = HTTParty.get('https://poloniex.com/public?command=returnOrderBook&currencyPair=ETH_OMG&depth=10')
       rescue Errno::ETIMEDOUT
-        poloniex_response = HTTParty.get('https://poloniex.com/public?command=returnOrderBook&currencyPair=ETH_OMG&depth=10')
+        puts "poloniex rescue"
+        retry
       end
       puts "///// #{x} ///////"
       x -= 1;
